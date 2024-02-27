9974c99cd36dc07f8354effb575d605590cf8195
Fayra Miranda <52434685+MirandaFayra@users.noreply.github.com>
2024-02-27 20:31:35 -0300

    adicionar exemplos css

---
+++ 02_FRONTEND/A7_CSS_2/CSS-exemplos/border/border-color/style.css
@@ -0,0 +1,14 @@
+p.one {
+  border-style: solid;
+  border-color: red;
+}
+
+p.two {
+  border-style: solid;
+  border-color: green;
+}
+
+p.three {
+  border-style: dotted;
+  border-color: blue;
+}
