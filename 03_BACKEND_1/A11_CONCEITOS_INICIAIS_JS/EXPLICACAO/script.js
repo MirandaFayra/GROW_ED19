18801564d62ab00f0d41f884ca4c2eaebf2789e2
Fayra Miranda <52434685+MirandaFayra@users.noreply.github.com>
2024-03-07 18:01:13 -0300

    Add arquivos de backend 1

---
+++ 03_BACKEND_1/A11_CONCEITOS_INICIAIS_JS/EXPLICACAO/script.js
@@ -0,0 +1,17 @@
+//-------- COMENTÁRIO ----
+
+//-------- ALERT ---------
+
+//------ PROMPT -----------
+
+//------ CONSOLE ----------
+
+//------ TIPOS DE DADOS ------
+
+// ----- OPERADORES ARITIMÉTICOS -----
+
+//------- OPERADORES DE ATRIBUICAO -----
+
+//-------- OPERADORES RELACIONAIS --------
+
+//-------- CONDICIONAIS --------
---
+++ 03_BACKEND_1/A11_CONCEITOS_INICIAIS_JS/EX_1/script.js
@@ -0,0 +1,12 @@
+/* 
+
+Desenvolva um algoritmo que calcule a média ponderada de um aluno e no final informe se o aluno passou se tirou mais que 7 ou que reprovou se tirou menos que 7. O aluno terá 3 notas e cada nota terá os seguintes pesos: 2, 3 e 5 respectivamente
+
+
+- Fazer o Diagrama 
+
+
+- Fazer o código
+
+
+*/
