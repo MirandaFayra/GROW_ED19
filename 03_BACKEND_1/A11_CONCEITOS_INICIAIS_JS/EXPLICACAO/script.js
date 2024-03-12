8314c57edb49be0cf408e5af51402fe0a879e0ad
Fayra Miranda <52434685+MirandaFayra@users.noreply.github.com>
2024-03-12 18:09:52 -0300

    Finalizar template aula condicionais

--- 03_BACKEND_1/A11_CONCEITOS_INICIAIS_JS/EXPLICACAO/script.js
+++ 03_BACKEND_1/A11_CONCEITOS_INICIAIS_JS/EXPLICACAO/script.js
@@ -1,3 +1,5 @@
+console.log('Olá mundo! ')
+
 //-------- COMENTÁRIO ----
 
 // - comentário de linha 
@@ -10,18 +12,193 @@
 
 //------ TIPOS DE DADOS ------
 
-let tipoString = 'Conjunto de caracteres 123 # % & > <'
+let tipoString = "Conjunto de caracteres 123 # % & > <"
 
-//-------- ALERT ---------
+let tipoNumero = 123 
 
-//------ PROMPT -----------
+let tipoNumeroFlutuante = 1.5 
+
+let tipoBoleano = true 
+
 
 //------ CONSOLE ----------
 
+// Para verificar o tipo => typeOf
+
+//console.log(tipoString)
+
+//console.log(tipoNumero)
+
+//console.log(tipoNumeroFlutuante)
+
+//console.log(tipoBoleano)
+
+
+//-------- ALERT ---------
+
+// alert('Aqui é a mensagem do alert')
+// alert(tipoNumero)
+
+
+//------ PROMPT -----------
+
+//let dadoPrompt = prompt('Digite o seu nome')
+
+//console.log(dadoPrompt)
+
+
 // ----- OPERADORES ARITIMÉTICOS -----
+
+let numero1 = 29 
+let numero2 = 10
+let numero3 = 2 
+
+let soma1 = numero1 + numero2
+let subtracao1 = numero2 - numero3
+let multiplicacao1 = numero3 * numero1
+let resto1 = numero2%numero3
+let resto2 = numero1%numero3
+let potencia1 =  numero2 ** numero3
+let potencia2 = numero3 ** numero2
+let potencia3 = potencia1 ** 5
+let testePrecedencia = 5+3 * 7
+let testePrecedencia2 = (5+3) * 7
+let divisao1 = numero2/numero3
+let divisao2 = numero2/numero2
+let divisao3 = numero3/0
+
+/* 
+    POTENCIA 1 
+    10 * 10 => 100
+
+    POTENCIA 2
+    2 * 2 * 2 * 2 * 2 * 2 *2 *2 *2 *2 
+
+*/
+
+//console.log(soma1)
+//console.log(subtracao1)
+//console.log(multiplicacao1)
+//console.log(resto1)
+//console.log(resto2) // 29 / 2 => dá 14 e sobra 1 
+//console.log(potencia1)
+//console.log(potencia2)
+//console.log(potencia3)
+
+//console.log(testePrecedencia)
+//console.log(testePrecedencia2)
+//console.log(divisao1)
+//console.log(divisao2)
+//console.log(divisao3) // tarefa de casa descobrir o por que 
+
 
 //------- OPERADORES DE ATRIBUICAO -----
 
+//let valor = 'Atribuindo valor'
+
+//let atribuicao = 'atribuicao'
+
+
+//x =- y  // =>  x = x - y
+//x *= y  // =>  x = x * 
+//x /= y  // =>  x = x / y
+//x %= y  // =>  x = x % y
+//x += 1  // =>  x = x + 1
+
+
 //-------- OPERADORES RELACIONAIS --------
 
+let n1 = 10 
+let n2 = 29 
+let n3 = 66
+let n4 = 77
+let n5 = "10"
+
+let operacao1 = n1 > n2 
+let operacao2 = n3 < n2 
+let operacao3 = n3 == n4 
+let operacao4 = n4 <= n2 
+let operacao5 = n3 >= n4
+let operacao6 = n3 != n4 
+let operacao7 = n3 != n3
+let operacao8 = n5 == n1
+let operacao9 = n5 === n1
+
+//console.log(operacao1)
+//console.log(operacao2)
+//console.log(operacao3)
+//console.log(operacao4)
+//console.log(operacao5)
+//console.log(operacao6)
+//console.log(operacao7)
+//console.log (operacao8)
+//console.log (operacao9)
+
+
+
 //-------- CONDICIONAIS --------
+
+/* 
+    SE FOR A PILULA AZUL =>  VOCÊ CONTINUA SONHANDO
+    
+    SE FOR A PILULA VERMELHA  =>  QUERIDÃO , ACORDA PRA REALIDADE 
+
+*/
+
+let pilulaAzulMatrix = false 
+let pilulaVermelha = false
+
+
+/*
+if(pilulaAzulMatrix){
+    console.log('VOCÊ CONTINUA SONHANDO')
+}
+*/
+
+/*
+if(pilulaVermelha){
+    console.log('QUERIDÃO , ACORDA PRA REALIDADE ')
+}
+*/
+
+/*
+if(!pilulaAzulMatrix){
+    console.log('VOCÊ CONTINUA SONHANDO')
+}
+*/
+
+
+
+/*
+if(pilulaVermelha){
+    console.log('QUERIDÃO , ACORDA PRA REALIDADE ')
+}
+*/
+
+/*
+
+if(!pilulaVermelha){
+    console.log('QUERIDÃO , ACORDA PRA REALIDADE ')
+}
+*/
+
+
+
+
+/*
+if(pilulaVermelha){
+    console.log('QUERIDÃO , ACORDA PRA REALIDADE ')
+}else {
+    console.log('VOCÊ CONTINUA SONHANDO ')
+}
+*/
+
+/*
+
+if(!pilulaVermelha){
+    console.log('QUERIDÃO , ACORDA PRA REALIDADE ')
+}else {
+    console.log('VOCÊ CONTINUA SONHANDO ')
+}
+
+*/
