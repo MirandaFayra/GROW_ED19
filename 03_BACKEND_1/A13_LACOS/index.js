1ea4f87410defe61a40e50f70b80548c36349fd1
Fayra Miranda <52434685+MirandaFayra@users.noreply.github.com>
2024-03-14 16:23:47 -0300

    Finalizar o template da aula

---
+++ 03_BACKEND_1/A13_LACOS/index.js
@@ -0,0 +1,35 @@
+//---------------- CONDICIONAIS --------
+
+//--------------- WHILE ----------------
+
+/* 
+
+    while (condição) {
+        // instruções
+    }
+*/
+
+//----------------- DO WHILE -------------
+
+/*
+
+    do {
+        // instruções
+    } while (condição);
+
+*/
+
+//----------------- FOR ----------------
+
+/* 
+    for (inicialização; condição; expressãoFinal) {
+        // instruções a serem executadas repetidamente
+    }
+
+*/
+
+
+//----------------- EX 1 ---------------
+
+
+//----------------- EX 2 ---------------
