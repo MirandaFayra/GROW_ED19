2208376efbd4e25dfc583d8aec768d8cb458155d
Fayra Miranda <52434685+MirandaFayra@users.noreply.github.com>
2024-03-14 22:17:56 -0300

    finalizar exs de aula

--- 03_BACKEND_1/A13_LACOS/index.js
+++ 03_BACKEND_1/A13_LACOS/index.js
@@ -1,5 +1,7 @@
 //---------------- CONDICIONAIS --------
 
+console.log("Olá mundo!")
+
 //--------------- WHILE ----------------
 
 /* 
@@ -9,15 +11,98 @@
     }
 */
 
+/*
+let salario = 1000
+
+while(salario < 5000){
+    salario += 100
+    console.log(`Seu salario é R$${salario}`)
+}
+*/
+
+
+/*
+let numero = Number(prompt("Digite um número"))
+
+while (numero<=20) {
+    console.log(`Esse é o número ${numero}`)
+    numero ++
+}
+*/
+
+
+
+
+/*
+let numero = 1
+let variavelA = numero++ 
+let variavelB = ++numero
+
+console.log(`Essa é a varivél A : ${variavelA}`)
+console.log(`Essa é a varivél B : ${variavelB}`)
+*/ 
+
+//---- 
+//let i 
+//let j 
+// let contador 
+
+
+
+
 //----------------- DO WHILE -------------
 
 /*
 
+    while (condição) {
+        // instruções
+    }
+
+    // Codigo executado pelo menos uma vez 
     do {
         // instruções
     } while (condição);
 
 */
+
+/*
+let contador = 4
+
+do {
+    console.log('Inicio')
+    console.log(`Contador : ${contador}`)
+    contador ++
+} while (contador<3)
+*/ 
+
+/*
+let aumento = Number(prompt("Digite seu aumento "))
+
+do {
+    console.log(`O valor do seu aumento :${aumento}`)
+    aumento += 50 
+
+} while (aumento<500)
+*/ 
+
+
+/*
+//AUMENTANDO COM PORCENTAGEM 
+
+let salario = Number(prompt("Digite seu salario "))
+let aumento = Number(prompt("Digite seu aumento "))
+let porcentagem = aumento/100
+
+do {
+    console.log(`O valor do seu aumento :${salario}`)
+    salario = salario* (1+porcentagem)
+
+} while (salario<500)
+
+*/
+
+
+
 
 //----------------- FOR ----------------
 
@@ -29,7 +114,44 @@
 */
 
 
+/*
+var passo 
+
+for (passo = 0; passo < 5; passo++) {
+  // Executa 5 vezes, com os valores de passos de 0 a 4.
+  console.log("Ande um passo para o leste")
+}
+*/ 
+
+/*
+for (let contador = 0; contador < 9; contador++) {
+    console.log(contador)
+}
+*/ 
+
+/*
+for (let i = 0 ; i<=100; i+=2){
+
+    if(i ===7){
+        break
+    }
+    console.log(`Número total de gatos : ${i}`)
+}
+*/ 
+
+let i = 0
+
+for (i ; i<=100; i+=3){
+    console.log(`Número total de gatos : ${i}`)
+}
+
+  
+
+
+
 //----------------- EX 1 ---------------
 
 
 //----------------- EX 2 ---------------
+
+
