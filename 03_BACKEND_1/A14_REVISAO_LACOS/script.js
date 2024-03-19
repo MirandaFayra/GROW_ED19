9a0702fdcf48300f2a247dfbf618aa032d00109b
Fayra Miranda <52434685+MirandaFayra@users.noreply.github.com>
2024-03-19 16:42:50 -0300

    Finalizar base

---
+++ 03_BACKEND_1/A14_REVISAO_LACOS/script.js
@@ -0,0 +1,65 @@
+/*
+    LAÇOS 
+
+
+    while (condição) {
+        // instruções
+    }
+
+
+    ///-----
+
+    do {
+        // instruções
+    } while (condição);
+
+
+    ///-----
+
+
+     for (inicialização; condição; expressãoFinal) {
+        // instruções a serem executadas repetidamente
+    }
+
+*/
+
+
+// --------  EXERCÍCIO 1 ----------------- 
+
+/* O fim do ano Astrológico chegou e com ele, vem a  nossa querida contagem regressiva para um novo ano astrologico. 
+
+Pergunte para a pessoa usuária quantos dias faltam para o ano novo atrológico e imprima a seguinte frase 
+
+"Faltam 10 dias para o ano novo atrológico. Prepare suas simpatias"
+
+"Faltam 9 dias para o ano novo atrológico. Prepare suas simpatias"
+*/
+
+
+
+
+
+//--------- EXERCICIO 2 -----------------
+
+/* 
+
+Você decidiu calcular quantos dias você vai praticar programação ao longo de uma semana. Escreva um programa que solicite ao usuário a quantidade de dias de código praticados por dia e, em seguida, calcule e imprima a quantidade total de dias de código praticados em uma semana (considerando que todos os dias têm a mesma quantidade de dias de código praticados).
+
+*/
+
+
+
+//--------- EXERCICIO 3 -----------------
+
+/*
+    Crie um algoritmo que leia o valor inicial da contagem, o valor final e o incremento, mostrando em seguida todos os valores no intervalo:
+    Ex: 
+    Digite o primeiro Valor: 3 
+    Digite o último Valor: 10 
+    Digite o incremento: 2 
+    Contagem: 3 5 7 9 Acabou!
+
+*/
+
+
+
