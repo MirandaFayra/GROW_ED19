983acfc41e7e12ca8fa62854fc37c8be83ad7a5f
Fayra Miranda <52434685+MirandaFayra@users.noreply.github.com>
2024-03-26 18:07:44 -0300

    Ajustar exxercicio final

---
+++ 03_BACKEND_1/A15_OBJETOS/scripts.js
@@ -0,0 +1,53 @@
+//----------------- OBJETO ------------- 
+
+/* 
+    - Caracteristicas
+
+    - Armazenar valores complexos 
+
+    - Chave e Valor 
+
+*/
+
+
+//--------------  INICIALIZANDO OBJETO -------------
+
+
+//----------  ATRIBUINDO VALORES OBJETO -------------
+
+
+//-----------------  LER OS DADOS ------------------- 
+
+
+//------------ INSERIR NOVA PROPRIEDADE ----------------
+
+
+//------------ MUDAR VALOR DE PROPRIEDADE ---------------
+
+
+//----------  CRIAR UM OBJETO BASEADO EM OUTRO ------------
+
+//---------- INSERIR UM NOVO ELEMENTO EM UMA LISTA ---------
+
+//---------- PERCORRENDO UMA LISTA DE OBJETOS ---------
+
+//------------------ EXERCICIO FIXAÇÃO --------------
+
+/*
+    Crie dois objetos contendo as seguintes propriedades e realize os itens : 
+
+    * Nome do objeto 
+    * Preco do objeto 
+    * Preco após desconto
+    * Insira esses objetos em uma lista de produtos 
+    
+    a-) Imprima a média do preço do objeto 
+    b-) Imprima a média de preço após desconto 
+    c-) Crie um novo produto a partir do segundo produto, com nova média e valores
+    d-)Insira os 3 produtos em uma lista de produtos
+    e-) Se a média de preço é menor que 50, imprimir "Estamos com desconto, acesse nosso site"
+    f-) Imprima o nome todos os itens da lista de produto
+
+
+*/
+
