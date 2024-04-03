a75a9799b94f374e59f883b1b4d3fc7d609ccad8
Fayra Miranda <52434685+MirandaFayra@users.noreply.github.com>
2024-04-02 21:26:48 -0300

    Adicionar resolucao

--- 03_BACKEND_1/A18_REVISAO_BACKEND_2/PARTE1/PARTE1_COND/script.js
+++ 03_BACKEND_1/A18_REVISAO_BACKEND_2/PARTE1/PARTE1_COND/script.js
@@ -81,5 +81,61 @@ switch (clima) {
     break;
 }
 
+/* 
+
+EXERCICIO 1 
+
+Uma pizzaria oferece três tamanhos de pizza:
+
+Pequena: R$20,00
+Média: R$30,00
+Grande: R$40,00
 
 
+Se o cliente pedir uma pizza pequena e refrigerante, o total será R$25,00..
+
+Escreva um programa que leia o tamanho da pizza (pequena, média ou grande) e a resposta do cliente sobre o refrigerante (sim ou não). O programa deve imprimir o valor total a ser pago pelo cliente.
+
+
+*/
+
+
+/*
+
+Estamos desenvolvendo o maior E-commerce da América Latina. Para isso, estamos desenvolvendo um sistema que cadastra nossos produtos e serviços , onde devemos solicitar a pessoa que está cadastrando os produtos , as seguintes informações : 
+
+
+ - Id (identificador único) - Número inteiro;
+
+- Nome do Produto ; 
+
+- Preço do Produto (não pode ser negativo e nem menor que 20 reais) ; 
+
+- Quantidade em Estoque (deve ser maior que 10 e par) ; 
+
+
+Crie um programa que solicite a pessoa usuária cada uma das informações individualmente.
+
+Caso a pessoa usuária não passe as informações, retorne as seguintes mensagens em tela : 
+
+
+- ID =>  " Identificador não encontrado, passe um número válido " 
+
+- Nome do Produto => "Insira um nome de produto válido "
+
+- Preço do Produto =>  "Insira um preço de produto válido "
+
+- Quantidade em Estoque  => "Insira uma quantidade  de produto válida para o estoque "
+
+Após verificar cada um dos itens e ter certeza que todos foram preenchidos, precisamos verificar o valor do produto para conseguir determinar em qual categoria vai.
+
+Se o valor do produto for par, devemos encaminhar o produto para a lista de promoção especial e mostrar na tela a seguinte informação : 
+
+" O produto NOME_DO_PRODUTO  foi cadastrado com sucesso! Produto adicionado na nossa lista de promoções especiais "
+
+
+*/
+
+
+
+console.log('alteracao')
