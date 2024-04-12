e85c032b07a99b2935650053f72a90ff8e03a7f9
Fayra Miranda <52434685+MirandaFayra@users.noreply.github.com>
2024-04-11 23:05:10 -0300

    Adicionar os exercicios

---
+++ 03_BACKEND_1/A20_NODE_API/src/somaNumeros.js
@@ -0,0 +1,8 @@
+let numero1 = 5 
+let numero2 = 10 
+let soma = numero1 + numero2 
+
+console.log(`A soma dos números é ${soma}`)
+console.log('bananinha')
+
+console.log('Salveeee')
---
+++ 03_BACKEND_1/A20_NODE_API/src/subtracaoNumeros.js
@@ -0,0 +1,6 @@
+let numero1 = 5 
+let numero2 = 10 
+let subtracao = numero1 - numero2 
+
+console.log(`A subtracao dos números é ${subtracao}`)
+console.log('bananinha')
